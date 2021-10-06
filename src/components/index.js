1c45488f88bb5b9a77eb0b2d1f42a35bd64d49d2
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2021-10-05 22:47:04 -0400

    Remove unused export

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
-export { default as Gameboard } from "./GameBoard";
 export { default as ScoreBoard } from "./ScoreBoard";
 export { default as Cards } from "./Cards";
 export { default as Card } from "./Card";
