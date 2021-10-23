df506b69441a376b6c340038b784c906dae6b504
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2021-10-23 19:21:29 -0400

    🍎 Add fruits deck

---
+++ src/decks/fruits/index.js
@@ -0,0 +1,20 @@
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable comma-spacing */
+/* eslint-disable no-multi-spaces */
+
+// Attribution: https://www.freepik.com/free-vector/sporty-fruits-set-cartoon-pear-apple-avocado-strawberry-doing-yoga-running-lifting-weight-gym-flat-vector-illustrations-healthy-food-wellness-lifestyle-concept_10613646.htm#page=1&query=Fruit%20Illustrations&position=25&from_view=search
+
+export default {
+  id: "fruits",
+  title: "Fruits",
+  cards: [
+    { filename: "apple.png"      , name: "apple"      , backgroundColor: "#4363d8" },
+    { filename: "avacado.png"    , name: "avacado"    , backgroundColor: "#3cb44b" },
+    { filename: "banana.png"     , name: "banana"     , backgroundColor: "#ffe119" },
+    { filename: "lenon.png"      , name: "lenon"      , backgroundColor: "#e6194b" },
+    { filename: "pear.png"       , name: "pear"       , backgroundColor: "#f58231" },
+    { filename: "pineapple.png"  , name: "pineapple"  , backgroundColor: "#911eb4" },
+    { filename: "strawberry.png" , name: "strawberry" , backgroundColor: "#46f0f0" },
+    { filename: "watermelon.png" , name: "watermelon" , backgroundColor: "#f032e6" },
+  ],
+};
--- src/decks/index.js
+++ src/decks/index.js
@@ -1,9 +1,11 @@
 import favoriteCharacters from "./favorite-characters";
 import birds from "./birds";
+import fruits from "./fruits";
 
 const availableDecks = [
   birds,
   favoriteCharacters,
+  fruits,
 ];
 
 export default availableDecks;
