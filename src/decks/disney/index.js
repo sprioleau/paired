6101da3f82ed92739b8b423d8f9c68a9b13c4942
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2021-10-25 07:10:29 -0400

    Add Disney Characters deck

---
+++ src/decks/disney/index.js
@@ -0,0 +1,22 @@
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable comma-spacing */
+/* eslint-disable no-multi-spaces */
+
+export default {
+  id: "disney-characters",
+  title: "Disney Characters",
+  cards: [
+    { filename: "mulan.gif"                 , name: "mulan"                 , backgroundColor: "#ffe119" },
+    { filename: "abu.gif"                   , name: "abu"                   , backgroundColor: "#3cb44b" },
+    { filename: "mushu.gif"                 , name: "mushu"                 , backgroundColor: "#4363d8" },
+    { filename: "aladdin-and-jasmine.gif"   , name: "aladdin-and-jasmine"   , backgroundColor: "#e6194b" },
+    { filename: "olaf.gif"                  , name: "olaf"                  , backgroundColor: "#f58231" },
+    { filename: "anna.gif"                  , name: "anna"                  , backgroundColor: "#911eb4" },
+    { filename: "pocahontas.gif"            , name: "pocahontas"            , backgroundColor: "#46f0f0" },
+    { filename: "genie.gif"                 , name: "genie"                 , backgroundColor: "#f032e6" },
+    { filename: "princess-jasmine.gif"      , name: "princess-jasmine"      , backgroundColor: "#bcf60c" },
+    { filename: "lumiere-and-cogsworth.gif" , name: "lumiere-and-cogsworth" , backgroundColor: "#fabebe" },
+    { filename: "princess-tiana.gif"        , name: "princess-tiana"        , backgroundColor: "#abcdff" },
+    { filename: "moana.gif"                 , name: "moana"                 , backgroundColor: "#99ffaa" },
+  ],
+};
--- src/decks/index.js
+++ src/decks/index.js
@@ -1,11 +1,13 @@
 import favoriteCharacters from "./favorite-characters";
 import birds from "./birds";
 import fruits from "./fruits";
+import disney from "./disney";
 
 const availableDecks = [
   birds,
   favoriteCharacters,
   fruits,
+  disney,
 ];
 
 export default availableDecks;
