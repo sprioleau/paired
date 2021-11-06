8778eeeb9bca6b0e534e6de49dcc1b67a45ba8a7
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2021-11-06 17:35:24 -0400

    Add Cocomelon deck

---
+++ src/decks/cocomelon/index.js
@@ -0,0 +1,20 @@
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable comma-spacing */
+/* eslint-disable no-multi-spaces */
+
+export default {
+  id: "cocomelon",
+  title: "Cocomelon",
+  cards: [
+    { filename: "cocomelon-1.gif" , name: "cocomelon-1" , backgroundColor: "#46f0f0" },
+    { filename: "cocomelon-2.gif" , name: "cocomelon-2" , backgroundColor: "#3cb44b" },
+    { filename: "cocomelon-3.gif" , name: "cocomelon-3" , backgroundColor: "#ffe119" },
+    { filename: "cocomelon-4.gif" , name: "cocomelon-4" , backgroundColor: "#4363d8" },
+    { filename: "cocomelon-5.gif" , name: "cocomelon-5" , backgroundColor: "#f58231" },
+    { filename: "cocomelon-6.gif" , name: "cocomelon-6" , backgroundColor: "#e6194b" },
+    { filename: "cocomelon-7.gif" , name: "cocomelon-7" , backgroundColor: "#46f0f0" },
+    { filename: "cocomelon-8.gif" , name: "cocomelon-8" , backgroundColor: "#f032e6" },
+    { filename: "cocomelon-9.gif" , name: "cocomelon-9" , backgroundColor: "#bcf60c" },
+    { filename: "cocomelon-10.gif", name: "cocomelon-10", backgroundColor: "#fabebe" },
+  ],
+};
--- src/decks/index.js
+++ src/decks/index.js
@@ -4,6 +4,7 @@ import fruits from "./fruits";
 import disney from "./disney";
 import pokemon from "./pokemon";
 import numbersZeroTen from "./numbers-zero-ten";
+import cocomelon from "./cocomelon";
 
 const availableDecks = [
   birds,
@@ -12,6 +13,7 @@ const availableDecks = [
   disney,
   pokemon,
   numbersZeroTen,
+  cocomelon,
 ];
 
 export default availableDecks;
