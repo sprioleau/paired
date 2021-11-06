b58896932e925a821c66b890305bde7702df2ccc
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2021-11-06 08:44:30 -0400

    Add Pokémon deck

--- src/decks/index.js
+++ src/decks/index.js
@@ -2,12 +2,14 @@ import favoriteCharacters from "./favorite-characters";
 import birds from "./birds";
 import fruits from "./fruits";
 import disney from "./disney";
+import pokemon from "./pokemon";
 
 const availableDecks = [
   birds,
   favoriteCharacters,
   fruits,
   disney,
+  pokemon,
 ];
 
 export default availableDecks;
---
+++ src/decks/pokemon/index.js
@@ -0,0 +1,24 @@
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable comma-spacing */
+/* eslint-disable no-multi-spaces */
+
+// source: https://pixabay.com/vectors/birdids-branch-children-4091989
+
+export default {
+  id: "pokemon",
+  title: "Pokémon",
+  cards: [
+    { filename: "pikachu-run.gif" , name: "pikachu-run" , backgroundColor: "#f032e6" },
+    { filename: "bulbasaur-8bit.gif" , name: "bulbasaur-8bit" , backgroundColor: "#46f0f0" },
+    { filename: "bulbasaur.gif" , name: "bulbasaur" , backgroundColor: "#3cb44b" },
+    { filename: "cubone.gif" , name: "cubone" , backgroundColor: "#ffe119" },
+    { filename: "dratini.gif" , name: "dratini" , backgroundColor: "#4363d8" },
+    { filename: "dugtrio.gif" , name: "dugtrio" , backgroundColor: "#f58231" },
+    { filename: "evie.gif" , name: "evie" , backgroundColor: "#e6194b" },
+    { filename: "mew.gif" , name: "mew" , backgroundColor: "#46f0f0" },
+    { filename: "pikachu-wink.gif" , name: "pikachu-wink" , backgroundColor: "#bcf60c" },
+    { filename: "snorlax.gif", name: "snorlax", backgroundColor: "#fabebe" },
+    { filename: "squirtle.gif", name: "squirtle", backgroundColor: "#3cb44b" },
+    { filename: "togepi.gif", name: "togepi", backgroundColor: "#911eb4" },
+  ],
+};
