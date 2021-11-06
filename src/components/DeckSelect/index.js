8954ef329f363294787231f89f1357a538107d08
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2021-11-06 09:38:51 -0400

    Select random card for cover

--- src/components/DeckSelect/index.js
+++ src/components/DeckSelect/index.js
@@ -3,6 +3,7 @@ import { useHistory } from "react-router-dom";
 import { selectSelectDeck, selectResetGame } from "../../store/selectors";
 import useStore from "../../store";
 import availableDecks from "../../decks";
+import selectRandomElement from "../../utlis/selectRandomElement";
 
 const DeckSelect = () => {
   const selectDeck = useStore(selectSelectDeck);
@@ -20,25 +21,29 @@ const DeckSelect = () => {
     <div className="deck-select">
       <h2 className="deck-select__prompt">Select a deck!</h2>
       <ul className="deck-select__decks">
-        {availableDecks.map(({ id, title, cards }) => (
-          <li key={id} className="deck-select__deck" onClick={() => handleSelectDeck(id)}>
-            <h3 className="deck-select__title">{title}</h3>
-            <div className="deck-select__cover-image-wrapper">
-              <div
-                className="deck-select__cover-image"
-                alt={cards[0].name}
-                data-name={cards[0].name}
-                style={{
-                  backgroundImage: `url(images/${id}/${cards[0].filename})`,
-                  backgroundSize: "cover",
-                  backgroundPosition: "center",
-                  backgroundRepeat: "no-repeat",
-                  backgroundColor: cards[0].backgroundColor,
-                }}
-              />
-            </div>
-          </li>
-        ))}
+        {availableDecks.map(({ id, title, cards }) => {
+          const randomCard = selectRandomElement(cards);
+
+          return (
+            <li key={id} className="deck-select__deck" onClick={() => handleSelectDeck(id)}>
+              <h3 className="deck-select__title">{title}</h3>
+              <div className="deck-select__cover-image-wrapper">
+                <div
+                  className="deck-select__cover-image"
+                  alt={randomCard.name}
+                  data-name={randomCard.name}
+                  style={{
+                    backgroundImage: `url(images/${id}/${randomCard.filename})`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                    backgroundRepeat: "no-repeat",
+                    backgroundColor: randomCard.backgroundColor,
+                  }}
+                />
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
--- src/utlis/index.js
+++ src/utlis/index.js
@@ -1,3 +1,4 @@
 export { default as generateDeckFromData } from "./generateDeckFromData";
 export { default as timeout } from "./timeout";
 export { default as randomBetween } from "./randomBetween";
+export { default as selectRandomElement } from "./selectRandomElement";
