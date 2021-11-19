dca1232714cd83a8acb4ef21d1ad060b49d3e768
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2021-11-19 14:43:05 -0500

    Use arrow function

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/media-has-caption */
 import "../styles/styles.scss";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
@@ -8,28 +9,27 @@ import Toolbar from "./Toolbar";
 import GameFinished from "./GameFinished/index";
 import DeckSelect from "./DeckSelect/index";
 
-function App() {
-  return (
-    <div className="app">
-      <header className="main-header">
-        <h1>Paired</h1>
-        <ScoreBoard />
-      </header>
-      <main className="main-content">
-        <Router>
-          <Switch>
-            <Route exact path="/" component={DeckSelect} />
-            <Route exact path="/play">
-              <ConfettiBlast />
-              <GameFinished />
-              <Toolbar />
-              <Cards />
-            </Route>
-          </Switch>
-        </Router>
-      </main>
-    </div>
-  );
-}
+const App = () => (
+  <div className="app">
+    <header className="main-header">
+      <h1>Paired</h1>
+      <ScoreBoard />
+    </header>
+    <main className="main-content">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={DeckSelect} />
+          <Route exact path="/play">
+            <ConfettiBlast />
+            <GameFinished />
+            <Toolbar />
+            <Cards />
+          </Route>
+        </Switch>
+      </Router>
+    </main>
+    <audio src="/audio/game-sounds.mp3" preload="auto" data-game-sounds />
+  </div>
+);
 
 export default App;
