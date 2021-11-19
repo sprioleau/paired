5f60ddf54c50b74e2681276f1d16cd350d80efb3
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2021-11-19 14:39:18 -0500

    Add Lottie Animation on deck selection

--- src/components/DeckSelect/index.js
+++ src/components/DeckSelect/index.js
@@ -1,52 +1,83 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import Lottie from "lottie-react";
 import { useHistory } from "react-router-dom";
-import { selectSelectDeck, selectResetGame } from "../../store/selectors";
+import confettiBurst from "../../lottie/confettie-burst.json";
+import { selectSelectDeck } from "../../store/selectors";
 import useStore from "../../store";
 import availableDecks from "../../decks";
-import selectRandomElement from "../../utlis/selectRandomElement";
 
 const DeckSelect = () => {
-  const selectDeck = useStore(selectSelectDeck);
-  const resetGame = useStore(selectResetGame);
-
-  const history = useHistory();
-
-  const handleSelectDeck = (deckId) => {
-    selectDeck(deckId);
-    resetGame();
-    history.push("/play");
-  };
+  const [allowSelections, setAllowSelections] = useState(true);
 
   return (
     <div className="deck-select">
       <h2 className="deck-select__prompt">Select a deck!</h2>
-      <ul className="deck-select__decks">
-        {availableDecks.map(({ id, title, cards }) => {
-          const randomCard = selectRandomElement(cards);
-
-          return (
-            <li key={id} className="deck-select__deck" onClick={() => handleSelectDeck(id)}>
-              <h3 className="deck-select__title">{title}</h3>
-              <div className="deck-select__cover-image-wrapper">
-                <div
-                  className="deck-select__cover-image"
-                  alt={randomCard.name}
-                  data-name={randomCard.name}
-                  style={{
-                    backgroundImage: `url(images/${id}/${randomCard.filename})`,
-                    backgroundSize: "cover",
-                    backgroundPosition: "center",
-                    backgroundRepeat: "no-repeat",
-                    backgroundColor: randomCard.backgroundColor,
-                  }}
-                />
-              </div>
-            </li>
-          );
-        })}
+      <ul
+        className="deck-select__decks"
+        style={{ pointerEvents: allowSelections ? "auto" : "none" }}
+      >
+        {availableDecks.map((deck) => <DeckCover key={deck.id} deck={deck} setAllowSelections={setAllowSelections} />)}
       </ul>
     </div>
   );
 };
 
 export default DeckSelect;
+
+const DeckCover = ({
+ deck: {
+ id, title, cards, coverIndex,
+}, setAllowSelections,
+}) => {
+  const selectDeck = useStore(selectSelectDeck);
+  const history = useHistory();
+  const lottieRef = useRef();
+  const selectedCard = cards[coverIndex];
+
+  const handleAnimationComplete = () => history.push("/play");
+
+  const handleSelectDeck = (deckId) => {
+    setAllowSelections(false);
+    lottieRef.current.setSpeed(2);
+    lottieRef.current.play();
+    selectDeck(deckId);
+  };
+
+  return (
+    <li key={id} className="deck-select__deck" onClick={() => handleSelectDeck(id)}>
+      <h3 className="deck-select__title">{title}</h3>
+      <div className="deck-select__cover-image-wrapper">
+        <div
+          className="deck-select__cover-image"
+          alt={selectedCard.name}
+          data-name={selectedCard.name}
+          style={{
+            backgroundImage: `url(images/${id}/${selectedCard.filename})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            backgroundColor: selectedCard.backgroundColor,
+            position: "relative",
+          }}
+        >
+          <Lottie
+            animationData={confettiBurst}
+            loop={false}
+            autoplay={false}
+            onComplete={handleAnimationComplete}
+            lottieRef={lottieRef}
+            style={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              width: "225%",
+              height: "225%",
+              pointerEvents: "none",
+            }}
+          />
+        </div>
+      </div>
+    </li>
+  );
+};
