b0c1e27d54ced92ed27b4be9b307713513c64c50
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2021-11-19 22:50:58 -0500

    Remove cocomelon images due to their size

--- src/decks/index.js
+++ src/decks/index.js
@@ -4,7 +4,6 @@ import fruits from "./fruits";
 import disney from "./disney";
 import pokemon from "./pokemon";
 import numbersZeroTen from "./numbers-zero-ten";
-import cocomelon from "./cocomelon";
 
 const availableDecks = [
   birds,
@@ -13,7 +12,6 @@ const availableDecks = [
   disney,
   pokemon,
   numbersZeroTen,
-  cocomelon,
 ];
 
 export default availableDecks;
