521121384245ec708460f8ff586cc0a6831076c3
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2022-04-28 18:40:50 -0400

    ✨ Add new deck

--- src/decks/index.js
+++ src/decks/index.js
@@ -1,12 +1,14 @@
 import favoriteCharacters from "./favorite-characters";
-import birds from "./birds";
+// import birds from "./birds";
+import pawPatrol from "./paw-patrol";
 import fruits from "./fruits";
 import disney from "./disney";
 import pokemon from "./pokemon";
 import numbersZeroTen from "./numbers-zero-ten";
 
 const availableDecks = [
-  birds,
+  // birds,
+  pawPatrol,
   favoriteCharacters,
   fruits,
   disney,
---
+++ src/decks/paw-patrol/index.js
@@ -0,0 +1,21 @@
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable comma-spacing */
+/* eslint-disable no-multi-spaces */
+
+export default {
+  id: "paw-patrol",
+  title: "Paw Patrol",
+  coverIndex: 1,
+  cards: [
+    { filename: "paw-patrol-1.gif" , name: "paw-patrol-1" , backgroundColor: "#e6194b" },
+    { filename: "paw-patrol-2.gif" , name: "paw-patrol-2" , backgroundColor: "#3cb44b" },
+    { filename: "paw-patrol-3.gif" , name: "paw-patrol-3" , backgroundColor: "#ffe119" },
+    { filename: "paw-patrol-4.gif" , name: "paw-patrol-4" , backgroundColor: "#4363d8" },
+    { filename: "paw-patrol-5.gif" , name: "paw-patrol-5" , backgroundColor: "#f58231" },
+    { filename: "paw-patrol-6.gif" , name: "paw-patrol-6" , backgroundColor: "#911eb4" },
+    { filename: "paw-patrol-7.gif" , name: "paw-patrol-7" , backgroundColor: "#46f0f0" },
+    { filename: "paw-patrol-8.gif" , name: "paw-patrol-8" , backgroundColor: "#f032e6" },
+    { filename: "paw-patrol-9.gif" , name: "paw-patrol-9" , backgroundColor: "#bcf60c" },
+    { filename: "paw-patrol-10.gif", name: "paw-patrol-10", backgroundColor: "#fabebe" },
+  ],
+};
