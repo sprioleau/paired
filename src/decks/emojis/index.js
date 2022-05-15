3b55c5395b698878b69f999abd77d33c4d812244
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2022-05-15 14:12:20 -0400

    🧐 Add emoji deck

---
+++ src/decks/emojis/index.js
@@ -0,0 +1,23 @@
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable comma-spacing */
+/* eslint-disable no-multi-spaces */
+
+// source: https://pixabay.com/vectors/bird-kids-branch-children-4091989
+
+export default {
+  id: "emojis",
+  title: "Emojis",
+  coverIndex: 1,
+  cards: [
+    { filename: "emoji-1.gif" , name: "emoji-1" , backgroundColor: "#46f0f0" },
+    { filename: "emoji-2.gif" , name: "emoji-2" , backgroundColor: "#3cb44b" },
+    { filename: "emoji-3.gif" , name: "emoji-3" , backgroundColor: "#ffe119" },
+    { filename: "emoji-4.gif" , name: "emoji-4" , backgroundColor: "#4363d8" },
+    { filename: "emoji-5.gif" , name: "emoji-5" , backgroundColor: "#f58231" },
+    { filename: "emoji-6.gif" , name: "emoji-6" , backgroundColor: "#e6194b" },
+    { filename: "emoji-7.gif" , name: "emoji-7" , backgroundColor: "#46f0f0" },
+    { filename: "emoji-8.gif" , name: "emoji-8" , backgroundColor: "#f032e6" },
+    { filename: "emoji-9.gif" , name: "emoji-9" , backgroundColor: "#bcf60c" },
+    { filename: "emoji-10.gif", name: "emoji-10", backgroundColor: "#fabebe" },
+  ],
+};
--- src/decks/index.js
+++ src/decks/index.js
@@ -1,18 +1,20 @@
-import favoriteCharacters from "./favorite-characters";
 // import birds from "./birds";
+// import pokemon from "./pokemon";
+import favoriteCharacters from "./favorite-characters";
+import emojis from "./emojis";
 import pawPatrol from "./paw-patrol";
 import fruits from "./fruits";
 import disney from "./disney";
-import pokemon from "./pokemon";
 import numbersZeroTen from "./numbers-zero-ten";
 
 const availableDecks = [
   // birds,
-  pawPatrol,
+  // pokemon,
   favoriteCharacters,
+  emojis,
+  pawPatrol,
   fruits,
   disney,
-  pokemon,
   numbersZeroTen,
 ];
 
